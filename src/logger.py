950ea9371f32e2ca74d71266a44190dd2ea05a4e
Linkan Kumar Sahu <sahulinkan7@gmail.com>
2024-02-08 18:22:13 +0530

    added logger file code

--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,14 @@
 import logging
+import os,sys
 from datetime import datetime
 
+log_dir = "logs"
+log_file_name = f"log_{datetime.now().strftime('%d_%m_%y_%H_%M_%S')}.log"
+log_filepath = os.path.join(log_dir,log_file_name)
 
-logging.basicConfig(level=logging.INFO)
+os.makedirs(log_dir,exist_ok=True)
+
+logging.basicConfig(level=logging.INFO,
+                    filename=log_filepath,
+                    format="[%(asctime)s - %(name)s ] %(levelname)s : %(message)s"
+                    )
