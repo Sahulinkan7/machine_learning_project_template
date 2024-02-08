00b89a3a959c8f5c7383a6e551488e98b5f1d0b6
Linkan Kumar Sahu <sahulinkan7@gmail.com>
2024-02-08 13:11:52 +0530

    file structure for machine learning project added

---
+++ template.py
@@ -0,0 +1,44 @@
+import os,sys
+import logging
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO,format="[%(asctime)s] : %(message)s")
+
+list_of_files=[
+    f"src/__init__.py",
+    f"src/entity/__init__.py",
+    f"src/entity/artifact_entity.py",
+    f"src/entity/config_entity.py",
+    f"src/components/__init__.py",
+    f"src/components/data_ingestion.py",
+    f"src/components/data_transformation.py",
+    f"src/components/data_validation.py",
+    f"src/components/model_trainer.py",
+    f"src/pipeline/__init__.py",
+    f"src/pipeline/prediction_pipeline.py",
+    f"src/utils/__init__.py",
+    f"src/utils/commonutils.py",
+    f"src/constants/__init__.py",
+    f"src/exception.py",
+    f"src/logger.py",
+    f"config/config.yaml",
+    f"params.yaml",
+    f"setup.py",
+    f"dvc.yaml",
+]
+
+
+for file in list_of_files:
+    filepath = Path(file)
+    filedir,filename = os.path.split(filepath)
+    
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory : {filedir} for filename {filename}")
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"creating empty file {filepath}")
+    else:
+        logging.info(f"file {filepath} already exist, can not be created again.")
